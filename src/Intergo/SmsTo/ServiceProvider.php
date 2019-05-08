6e7d1618d1bffd6968a0aa01f0a23c980d865cc4
Manny Isles <manny.isles@gmail.com>
2019-05-09 06:30:38 +0800

    access token

--- src/Intergo/SmsTo/ServiceProvider.php
+++ src/Intergo/SmsTo/ServiceProvider.php
@@ -5,6 +5,8 @@ namespace Intergo\SmsTo;
 use Illuminate\Support\ServiceProvider as BaseServiceProvider;
 use Illuminate\Support\Facades\Blade;
 //use Intergo\SmsTo\SmsTo;
+use Carbon\Carbon;
+use Storage;
 use Intergo\SmsTo\Http\Client as SmsToClient;
 
 class ServiceProvider extends BaseServiceProvider {
@@ -29,11 +31,6 @@ class ServiceProvider extends BaseServiceProvider {
         );
 
         $this->loadViewsFrom(__DIR__.'/../../views', 'smsto');
-
-        // @error
-        // Blade::directive('error', function ($expression) {
-        //     return '';
-        // });
     }
 
     /**
@@ -49,12 +46,46 @@ class ServiceProvider extends BaseServiceProvider {
 
         // If PHP SDK is ready
         $this->app->bind('laravel-smsto', function() {
+            $accessToken = $this->getAccessToken();
             return new SmsToClient(
+                config('smsto.client_id'),
+                config('smsto.client_secret'),
+                config('smsto.username'),
+                config('smsto.password'),
+                $accessToken
+            );
+        });
+    }
+
+    public function getAccessToken()
+    {
+        $accessToken = null;
+        
+        // Check if we have accessToken saved already
+        if (!Storage::disk('local')->exists('smsto/accessToken')) {
+            $client = new SmsToClient(
                 config('smsto.client_id'),
                 config('smsto.client_secret'),
                 config('smsto.username'),
                 config('smsto.password')
             );
-        });
+            $response = $client->getAccessToken();
+
+            if ($response) {
+                if (isset($response['access_token'])) {
+                    $date = Carbon::now()->addSeconds($response['expires_in']);
+                    Storage::disk('local')->put('smsto/accessTokenExpiredOn', $date->toDateTimeString());
+                    Storage::disk('local')->put('smsto/accessToken', $response['access_token']);
+                    $accessToken = $response['access_token'];
+                }
+            }
+        } else {
+            $dateExpired = Storage::disk('local')->get('smsto/accessTokenExpiredOn');
+            if ($dateExpired > Carbon::now()->toDateTimeString()) {
+                $accessToken = Storage::disk('local')->get('smsto/accessToken');
+            }
+        }
+
+        return $accessToken;
     }
 }
