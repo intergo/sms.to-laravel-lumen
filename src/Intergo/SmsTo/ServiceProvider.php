55b9572845db7ff4871b65860f3003d5d666eb38
Manny Isles <manny.isles@gmail.com>
2019-05-09 06:34:14 +0800

    access token

--- src/Intergo/SmsTo/ServiceProvider.php
+++ src/Intergo/SmsTo/ServiceProvider.php
@@ -4,7 +4,6 @@ namespace Intergo\SmsTo;
 
 use Illuminate\Support\ServiceProvider as BaseServiceProvider;
 use Illuminate\Support\Facades\Blade;
-//use Intergo\SmsTo\SmsTo;
 use Carbon\Carbon;
 use Storage;
 use Intergo\SmsTo\Http\Client as SmsToClient;
@@ -40,10 +39,6 @@ class ServiceProvider extends BaseServiceProvider {
      */
     public function register()
     {
-        // $this->app->bind('laravel-smsto', function() {
-        //     return new SmsTo();
-        // });
-
         // If PHP SDK is ready
         $this->app->bind('laravel-smsto', function() {
             $accessToken = $this->getAccessToken();
@@ -60,9 +55,9 @@ class ServiceProvider extends BaseServiceProvider {
     public function getAccessToken()
     {
         $accessToken = null;
-        
+
         // Check if we have accessToken saved already
-        if (!Storage::disk('local')->exists('smsto/accessToken')) {
+        if ( ! Storage::disk('local')->exists('smsto/accessToken')) {
             $client = new SmsToClient(
                 config('smsto.client_id'),
                 config('smsto.client_secret'),
