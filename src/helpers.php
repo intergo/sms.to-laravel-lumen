ca72e294a5e317dcaa70a55b0a2bb53fc412224d
Manny Isles <manny.isles@gmail.com>
2019-05-10 01:22:25 +0800

    Update helpers.php

--- src/helpers.php
+++ src/helpers.php
@@ -3,6 +3,20 @@
 if (!function_exists('smsto')) {
 	function smsto()
 	{
-		return app(\SmsTo::class);
+		return app(\Intergo\SmsTo\SmsTo::class);
 	}
+}
+
+if ( ! function_exists('config_path'))
+{
+    /**
+     * Get the configuration path.
+     *
+     * @param  string $path
+     * @return string
+     */
+    function config_path($path = '')
+    {
+        return app()->basePath() . '/config' . ($path ? '/' . $path : $path);
+    }
 }
