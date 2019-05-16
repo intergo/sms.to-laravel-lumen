c21ad43d29202d75666a1bdfb422313c6a0cbdbd
Manny Isles <manny.isles@gmail.com>
2019-05-16 19:47:44 +0800

    Update LumenServiceProvider.php

--- src/Intergo/SmsTo/LumenServiceProvider.php
+++ src/Intergo/SmsTo/LumenServiceProvider.php
@@ -44,46 +44,13 @@ class LumenServiceProvider extends BaseServiceProvider {
     {
         // If PHP SDK is ready
         $this->app->bind('laravel-smsto', function() {
-            $accessToken = $this->getAccessToken();
             return new SmsToClient(
                 config('smsto.client_id'),
                 config('smsto.client_secret'),
                 config('smsto.username'),
                 config('smsto.password'),
-                $accessToken
+                app(\Intergo\SmsTo\Token::class)->getAccessToken()
             );
         });
-    }
-
-    public function getAccessToken()
-    {
-        $accessToken = null;
-
-        // Check if we have accessToken saved already
-        if ( ! Storage::disk('local')->exists('smsto/accessToken')) {
-            $client = new SmsToClient(
-                config('smsto.client_id'),
-                config('smsto.client_secret'),
-                config('smsto.username'),
-                config('smsto.password')
-            );
-            $response = $client->getAccessToken();
-
-            if ($response) {
-                if (isset($response['access_token'])) {
-                    $date = Carbon::now()->addSeconds($response['expires_in']);
-                    Storage::disk('local')->put('smsto/accessTokenExpiredOn', $date->toDateTimeString());
-                    Storage::disk('local')->put('smsto/accessToken', $response['access_token']);
-                    $accessToken = $response['access_token'];
-                }
-            }
-        } else {
-            $dateExpired = Storage::disk('local')->get('smsto/accessTokenExpiredOn');
-            if ($dateExpired > Carbon::now()->toDateTimeString()) {
-                $accessToken = Storage::disk('local')->get('smsto/accessToken');
-            }
-        }
-
-        return $accessToken;
     }
 }
