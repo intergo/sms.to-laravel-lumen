a10473fa4d1308de0b077b5df842efac66c1d0a5
Manny Isles <manny.isles@gmail.com>
2019-07-08 10:45:11 +0800

    Update smsto.php

--- src/config/smsto.php
+++ src/config/smsto.php
@@ -1,7 +1,6 @@
 <?php
 
 return [
-    'grant_type' => 'password',
     'client_id' => env('SMSTO_CLIENT_ID'),
     'client_secret' => env('SMSTO_CLIENT_SECRET'),
     'username'=> env('SMSTO_EMAIL'),
